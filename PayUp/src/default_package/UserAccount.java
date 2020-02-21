590504053cfaf5699a9f07298a64fa1b47c5e07e
gujonguyen <54783522+gujonguyen@users.noreply.github.com>
2020-02-21 11:25:47 +0100

    Update UserAccount.java

--- PayUp/src/default_package/UserAccount.java
+++ PayUp/src/default_package/UserAccount.java
@@ -1,12 +1,54 @@
 package default_package;
+import java.util.Scanner;
 
 public class UserAccount {
+	private String username;
+	private String password;
+	int userId;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int userId;
-		String userName;
-		String password;
-		
+public UserAccount(String username, String password) {
+	try (Scanner input = new Scanner (System.in)) {
+			this.setUsername(username);
+			this.setPassword(password);
+			
+			System.out.println("Welcome to your PayUp site!");
+			System.out.println("\nEnter your username and password to login to your account.");    
+
+			System.out.println("Username: ");
+			username = input.nextLine();
+
+			System.out.println("Password: ");
+			password = input.nextLine();
+		}  
+
+		UserAccount login = new UserAccount(username, password);
+
+		if(login.checkPassword())
+			System.out.println("You are logged in!");
+		else
+			System.out.println("The username and password you entered are incorrect.");
 	}
+
+	private boolean checkPassword() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }
+
