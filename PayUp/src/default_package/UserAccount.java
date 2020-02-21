82cecbb0a5e6f1ef5dc40f8e2f001e327c82d041
Sjoerd97 <61313847+Sjoerd97@users.noreply.github.com>
2020-02-21 12:18:42 +0100

    Update UserAccount.java

--- PayUp/src/default_package/UserAccount.java
+++ PayUp/src/default_package/UserAccount.java
@@ -1,54 +1,102 @@
 package default_package;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
 
 public class UserAccount {
-	private String username;
-	private String password;
 	int userId;
-
-public UserAccount(String username, String password) {
-	try (Scanner input = new Scanner (System.in)) {
-			this.setUsername(username);
-			this.setPassword(password);
-			
-			System.out.println("Welcome to your PayUp site!");
-			System.out.println("\nEnter your username and password to login to your account.");    
-
-			System.out.println("Username: ");
-			username = input.nextLine();
-
-			System.out.println("Password: ");
-			password = input.nextLine();
-		}  
-
-		UserAccount login = new UserAccount(username, password);
-
-		if(login.checkPassword())
-			System.out.println("You are logged in!");
-		else
-			System.out.println("The username and password you entered are incorrect.");
-	}
-
-	private boolean checkPassword() {
+	static String userName;
+	static String password;
+	static Scanner userInput1 = new Scanner(System.in);
+	
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		return false;
+		
+		int userChoice = getUserChoice();
+		switch (userChoice) {
+		case 1:
+		Register();
+		break;
+		case 2:
+		Login();
+		break;
+		}
+		}
+		// This method asks and returns what the user wants to do
+		
+	public static int getUserChoice(){
+		System.out.println("What do you wan to do?");
+		System.out.println("(1) Register your account");
+		System.out.println("(2) Login into account");
+		System.out.println("****************************************************************");
+		System.out.print("Please enter your choice (1 or 2): ");
+		return userInput1.nextInt();
 	}
+	
+	public static void Register() {
 
-	public String getUsername() {
-		return username;
+		String UserN_PassW;
+		
+		System.out.println("Please choose a username:");
+		userName = userInput1.nextLine();
+		System.out.println("Please choose a password:");
+		password = userInput1.nextLine();
+		
+		UserN_PassW = userName + "\t" + password;
+		
+		try { 
+			
+			PrintWriter wr = new PrintWriter( new BufferedWriter(new FileWriter("User_database",true)));
+			wr.println(UserN_PassW);
+				
+			wr.close();	
+			} catch (IOException e) {
+				System.out.println("I/O error when writing on file");
+			}
+		
 	}
+	
+	public static boolean Login() {
+		int lineNumber = 0;
+		String[] xaxis = new String[100];
+		String[] yaxis = new String[100];
+		
+		System.out.println("Please input your username:");
+		userName = userInput1.nextLine();
+		System.out.println("Please input your password:");
+		password = userInput1.nextLine();
+		
+		try {
+			String sCurrentLine;
+			String[] uCurrent = new String [2];
+			BufferedReader myFile = new BufferedReader (new FileReader("User_database.txt")); 
+			while ((sCurrentLine = myFile.readLine()) != null) {
+				uCurrent = sCurrentLine.split("\t");
 
-	public void setUsername(String username) {
-		this.username = username;
+				xaxis[lineNumber] = uCurrent[0];
+				yaxis[lineNumber] = uCurrent[1];
+				lineNumber++;
+			}
+			myFile.close(); 
+		}catch (IOException e) {
+				System.out.println("This file does not exist");
+		}
+		
+		String[] Finalxaxis = new String[lineNumber];
+		System.arraycopy(xaxis, 0, Finalxaxis, 0, lineNumber);
+		String[] Finalyaxis = new String[lineNumber];
+		System.arraycopy(xaxis, 0, Finalxaxis, 0, lineNumber);
+		System.arraycopy(yaxis, 0, Finalyaxis, 0, lineNumber);
+		
+		for (int i = 0; i < lineNumber; lineNumber++) {
+			if ( userName == Finalxaxis[i] && password == Finalyaxis[i]) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		
 	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
 }
+
 
