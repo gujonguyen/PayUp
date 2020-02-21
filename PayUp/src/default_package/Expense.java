170cb82cde90b0b7091d8ea4a212a6595ec40d51
526722ts <61310267+526722ts@users.noreply.github.com>
2020-02-21 11:22:14 +0100

    Add files via upload

--- PayUp/src/default_package/Expense.java
+++ PayUp/src/default_package/Expense.java
@@ -1,5 +1,15 @@
 package default_package;
+import java.util.*;
 
 public class Expense {
+	String expenseName;
+	Date expenseDate;
+	double amount;
+	List belongsToList = new List(); //every expense needs to belong to a list
+	List [] participants = new List[2];
 
+	public double splitExpense() {
+		double splitAmount = amount/participants.length;
+		return splitAmount;
+	}
 }
--- PayUp/src/default_package/UserAccount.java
+++ PayUp/src/default_package/UserAccount.java
@@ -4,7 +4,9 @@ public class UserAccount {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int userId;
+		String userName;
+		String password;
+		
 	}
-
 }
