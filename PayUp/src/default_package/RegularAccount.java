cabf532d5341afdaacb44587bc9902eb987f1731
526722ts <61310267+526722ts@users.noreply.github.com>
2020-02-24 12:43:34 +0100

    Update RegularAccount.java

--- PayUp/src/default_package/RegularAccount.java
+++ PayUp/src/default_package/RegularAccount.java
@@ -1,5 +1,17 @@
 package default_package;
 
 public class RegularAccount {
+	public static void getUserChoiceList() {
+		System.out.println("****************************************************************");
+		System.out.println("****************************************************************");
+		System.out.println("************\t\t What do you want to do? \t\t**********");
+		System.out.println("****************************************************************");
+		System.out.println("****************************************************************");
+		System.out.println("");
+		System.out.println("(1) Create new list");
+		System.out.println("(2) View List");
+		System.out.println("****************************************************************");
+		System.out.print("Please enter your choice (1 or 2): ");
+		userInputList.nextInt();	// gets either 1 or 2 from the user
 
 }
