fbd66af52f2562eeb0ad2e5c75ec87e26a15a307
Sjoerd97 <61313847+Sjoerd97@users.noreply.github.com>
2020-02-24 12:49:00 +0100

    Update RegularAccount.java

--- PayUp/src/default_package/RegularAccount.java
+++ PayUp/src/default_package/RegularAccount.java
@@ -1,6 +1,6 @@
 package default_package;
 
-public class RegularAccount {
+public class RegularAccount extends UserAccount {
 	public static void getUserChoiceList() {
 		System.out.println("****************************************************************");
 		System.out.println("****************************************************************");
@@ -12,6 +12,28 @@ public class RegularAccount {
 		System.out.println("(2) View List");
 		System.out.println("****************************************************************");
 		System.out.print("Please enter your choice (1 or 2): ");
-		userInputList.nextInt();	// gets either 1 or 2 from the user
+		userInputList.nextInt();	// gets either 1 or 2 from the use
 
+	
+	public void viewList() {
+		viewList();
+	}
+	public void createNewList() {
+		createNewList();
+	}
+	public void deleteList() {
+		deleteList();
+	}
+	public void settleList() {
+		settleList();
+	}
+	public void viewIndividualBalance() {
+		viewIndividualBalance();
+	}
+	public void viewExpenseHistory() {
+		viewExpenseHistory();
+	}
+	public void writeExpenseHistory() {
+		writeExpenseHistory();
+	}
 }
