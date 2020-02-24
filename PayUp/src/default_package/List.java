364f6a84779da7607f35f12a8393a0fe67c32def
446901yz <61310280+446901yz@users.noreply.github.com>
2020-02-24 12:20:14 +0100

    Update List.java

--- PayUp/src/default_package/List.java
+++ PayUp/src/default_package/List.java
@@ -1,5 +1,35 @@
 package default_package;
+import java.util.*;
 
 public class List {
+	static Scanner userInput1 = new Scanner(System.in);
 
+	public static void main(String[] args) {
+		int userChoice = getUserChoice1();
+	}
+
+	public static int getUserChoice1() {
+		System.out.println("****************************************************************");
+		System.out.println("****************************************************************");
+		System.out.println("************\t\t What do you want to do? \t\t**********");
+		System.out.println("****************************************************************");
+		System.out.println("****************************************************************");
+		System.out.println("");
+		System.out.println("(1) Create new list");
+		System.out.println("(2) View List");
+		System.out.println("****************************************************************");
+		System.out.print("Please enter your choice (1 or 2): ");
+		userInput1.nextInt();	// gets either 1 or 2 from the user
+
+	}
+	public static void errorChoice() {
+		int userChoice = getUserChoice1();
+		if (userChoice > 2) {
+			System.out.println("Your choice is invalid");
+
+		}
+		break;
+
+
+	}
 }
