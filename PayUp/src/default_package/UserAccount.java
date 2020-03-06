ad30bd67ce42a7bceb5c3ffac7c6471aadafb649
Sjoerd97 <61313847+Sjoerd97@users.noreply.github.com>
2020-03-06 15:13:04 +0100

    Update UserAccount.java

--- PayUp/src/default_package/UserAccount.java
+++ PayUp/src/default_package/UserAccount.java
@@ -1,118 +1,187 @@
 package default_package;
+import java.io.*;
+import java.util.*;
 
-import java.util.Scanner;
+public class UserAccount {
+	String userName;
+	String password;
+	String role;
+	static int noOfUsers;
+	int userID = 0;
+	private String sCurrentLine;
+	static Scanner userInput1 = new Scanner(System.in); //for int
+	static Scanner userInput2 = new Scanner(System.in); // for string
+	static Scanner userInput3 = new Scanner(System.in); // for double
+	int userChoice;
+	int loggedUserID;
+	
+	public UserAccount(String nameC, String passwordC, int uID ) {
+		userName = nameC;
+		password = passwordC;
+		userID = uID;
+	}
+	
+	public static UserAccount [] createAllUsers() {
+		int NumUser = 0;
+		String[] LocalUserName = new String[100];
+		String[] LocalPassword= new String[100];
+		String[] LocalTypeAccount = new String[100];
+		int[] LocalID = new int[100];
+		
+		UserAccount [] allUsers = new UserAccount [100];
 
-public class InterfaceClass {
-	static Scanner my_scan = new Scanner(System.in);
-	static Scanner my_scanINT = new Scanner(System.in); 
-	String typeOfAccount;
-	static int loggedUserID;
-	String[] passedUserData;
+		try {
+			String sCurrentLine;
+			String[] uCurrent = new String [4];
+			BufferedReader myFile = new BufferedReader (new FileReader("User_database.txt")); 
+			while ((sCurrentLine = myFile.readLine()) != null) {
+				uCurrent = sCurrentLine.split("\t");
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+				LocalUserName[NumUser] = uCurrent[0];
+				LocalPassword[NumUser] = uCurrent[1];
+				LocalTypeAccount[NumUser] = uCurrent[2];
+				LocalID[NumUser] = Integer.parseInt(uCurrent[3]);
+
+				NumUser++;
+			}
+			myFile.close(); 
+		}catch (IOException e) {
+			System.out.println("This file does not existlol5");
+		}
+		
+		String[] FinalLocalUserName = new String[NumUser];
+		System.arraycopy(LocalUserName, 0, FinalLocalUserName, 0, NumUser);
+		String[] FinalLocalPassword = new String[NumUser];
+		System.arraycopy(LocalPassword, 0, FinalLocalPassword, 0, NumUser);
+		String[] FinalLocalTypeAccount = new String[NumUser];
+		System.arraycopy(LocalTypeAccount, 0, FinalLocalTypeAccount, 0, NumUser);
+		int[] FinalLocalID = new int[NumUser];
+		System.arraycopy(LocalID, 0, FinalLocalID, 0, NumUser);
+		
+		
+		for (int i = 0; i < NumUser; i++) {
+			allUsers [i]  = new UserAccount(FinalLocalUserName[i], FinalLocalPassword[i], FinalLocalID[i]);
+		}
+		
+		return allUsers;
+	}
+	
+	
+	public UserAccount(int userChoicec, int loggedIDc) {
+	userChoice = userChoicec;
+	loggedUserID = loggedIDc;
+	switch (userChoice){
+	case 3:
+		Exit();
+		break;
+	}
+	}
+	public UserAccount() {
+		
+	}
+
+	protected static void Register() {
+
+		noOfUsers = readfile1();
+		System.out.println("--------------------------------------------------------");
+		System.out.println("\t\t Please Register User");
+		System.out.println("--------------------------------------------------------");
+		System.out.println("Are you registering as a (R) Regular or an (A) Administrator?");
+		String typeOfAccount = userInput2.nextLine();
+		System.out.println("Please choose a username:");
+		String Un = userInput2.nextLine();
+		System.out.println("Please choose a password:");
+		String Pw = userInput2.nextLine();
+		System.out.println("--------------------------------------------------------");
+		System.out.println("\t Thank you for registering to PayUp!");
+		System.out.println("--------------------------------------------------------");
+
+		try { //This is for Registration of the users
+			PrintWriter wr = new PrintWriter( new BufferedWriter(new FileWriter("User_database.txt",true)));
+			wr.println(Un + "\t" + Pw + "\t" + typeOfAccount + "\t" + noOfUsers);
+			wr.close();	
+		} catch (IOException e) {
+			System.out.println("I/O error when writing on file");
+		}
 		new InterfaceClass();
 	}
 
-	public InterfaceClass() {
-		int userChoice = getUserChoice1();
-		switch (userChoice) {
-		case 1:
-			UserAccount.Register();
+	protected static String[] Login() {
+		int NumUser = 0;
+		String[] LocalUserName = new String[100];
+		String[] LocalPassword = new String[100];
+		String[] LocalRole = new String[100];
+		int[] LocalID = new int[100];
+		int loggedID = 0;
+		String Un;
+		String Pw;
+		String role = "bla";
 
-		case 2:
-			String[] arrayofLoggedUser = UserAccount.Login();
-			typeOfAccount = arrayofLoggedUser[1];
-			int loggeduserIDl;
-			loggeduserIDl = Integer.parseInt(arrayofLoggedUser[0]);
-			if (typeOfAccount.equals("R") || typeOfAccount.equals("r") || typeOfAccount.equals("Regular") || typeOfAccount.equals("regular")){
-				RegularAccount(typeOfAccount, loggeduserIDl);
-				}
-			else if (typeOfAccount.equals("A") || typeOfAccount.equals("a")|| typeOfAccount.equals("Admin")|| typeOfAccount.equals("admin")) {
-				AdministratorAccount(typeOfAccount, loggeduserIDl);
+		System.out.println("--------------------------------------------------------");
+		System.out.println("\t\t Please Login User");
+		System.out.println("--------------------------------------------------------");
+		System.out.println("Please input your username:");
+		Un = userInput2.nextLine();
+		System.out.println("Please input your password:");
+		Pw = userInput2.nextLine();
+
+		try {
+			String sCurrentLine;
+			String[] uCurrent = new String [4];
+			BufferedReader myFile = new BufferedReader (new FileReader("User_database.txt")); 
+			while ((sCurrentLine = myFile.readLine()) != null) {
+				uCurrent = sCurrentLine.split("\t");
+
+				LocalUserName[NumUser] = uCurrent[0];
+				LocalPassword[NumUser] = uCurrent[1];
+				LocalRole[NumUser] = uCurrent[2];
+				LocalID[NumUser] = Integer.parseInt(uCurrent[3]);
+				NumUser++;
 			}
-			else {
-				System.out.println("Invalid login credentials, please try again!");
-				new InterfaceClass();
+			myFile.close(); 
+		}catch (IOException e) {
+			System.out.println("This file does not existlol1");
+		}
+
+		String[] FinalLocalUserName = new String[NumUser];
+		System.arraycopy(LocalUserName, 0, FinalLocalUserName, 0, NumUser);
+		String[] FinalLocalPassword = new String[NumUser];
+		System.arraycopy(LocalPassword, 0, FinalLocalPassword, 0, NumUser);
+		for (int i = 0; i < NumUser; i++) {
+			if (Un.equals(FinalLocalUserName[i]) && Pw.equals(FinalLocalPassword[i])) {
+				loggedID = LocalID[i];
+				role = LocalRole[i];
+				System.out.println("--------------------------------------------------------");
+				System.out.println("Login successful, welcome " + Un +"!");
 			}
-		case 3:
-			UserAccount.Exit();
-			break;
+			
 		}
+		String arrayofLoggedUser[] = new String[2];
+		arrayofLoggedUser[0] = Integer.toString(loggedID);
+		arrayofLoggedUser[1] = role;
+		
+		return arrayofLoggedUser;
+
 	}
 
-	private static int getUserChoice1(){
+	protected static void Exit() {
 		System.out.println("--------------------------------------------------------");
-		System.out.println("\t Welcome to PayUp! ");
-		System.out.println("\n What do you wish to?");   
-		System.out.println("--------------------------------------------------------");
-		System.out.println("(1) Register your account");
-		System.out.println("(2) Login into account");
-		System.out.println("(3) Exit");
-		System.out.println("--------------------------------------------------------");
-		System.out.print("Enter your option: ");
-		return my_scanINT.nextInt();
+		System.out.println("\tYou are successfully logged out\t");
 	}
 
-	public void RegularAccount(String typeOfAccountc, int loggedUserIDc) {
-		
-		typeOfAccount = typeOfAccountc;
-		
-		int loggedUserIDl = loggedUserIDc;
-		int userChoice = regularUserInterface();
-		
-		if (userChoice < 4) {
-			new List(userChoice, loggedUserIDl);
-		}else {
-			new RegularAccount(userChoice, loggedUserIDl);
+	public static int readfile1() {
+		int NumUser = 0;
+		try {
+			String sCurrentLine;
+			BufferedReader myFile = new BufferedReader (new FileReader("User_database.txt")); 
+			while ((sCurrentLine = myFile.readLine()) != null) {
+				NumUser++;
+			}
+			myFile.close(); 
+		}catch (IOException e) {
+			System.out.println("This file does not existlol2");
 		}
-
-	}
-
-	public static int regularUserInterface() {
-		System.out.println("--------------------------------------------------------");//user interface for regular users
-		System.out.println("You are logged in as a Regular user!");
-		System.out.println("What do you want to do?");
-		System.out.println("--------------------------------------------------------");
-		System.out.println("");
-		System.out.println("(1) Create a new list");
-		System.out.println("(2) View a list");
-		System.out.println("(3) View individual balance");
-		System.out.println("(4) Delete a list");
-		System.out.println("(5) Settle a list");
-		System.out.println("(6) Add Expense To List");
-		System.out.println("(7) View Expense History");
-		System.out.println("(8) Write Expense History");
-		System.out.println("(9) Logout");
-		System.out.println("--------------------------------------------------------");
-		System.out.print("You want to: ");
-		return my_scanINT.nextInt();	//getting user choice 
-	}
-
-	public void AdministratorAccount(String typeOfAccountc, int loggedUserIDc){
-		typeOfAccount = typeOfAccountc;
-		int loggedUserIDl = loggedUserIDc;
-		int userChoice = AdminInterface();
-		if (userChoice < 3) {
-			new AdministratorAccount(userChoice, loggedUserIDl);
-		}else {
-			new UserAccount(userChoice, loggedUserIDl);
-		}
-	}
-
-	static int AdminInterface() {
-
-		System.out.println("--------------------------------------------------------");
-		System.out.println("Welcome Admin of PayUp!");
-		System.out.println("You are logged in to the Administrator Account!");
-		System.out.println("--------------------------------------------------------");
-		System.out.println("What do you wish to do?");
-		System.out.println("(1) Remove a User from PayUp");
-		System.out.println("(2) Remove List");
-		System.out.println("(3) Logout");
-		System.out.println("--------------------------------------------------------");
-		System.out.print("Please enter your choice: ");
-		return  my_scanINT.nextInt();
+		return NumUser;
 	}
 
 }
