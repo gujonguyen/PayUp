a95f36a171a6e73c008f541ec8b75081705d7b1a
446901yz <61310280+446901yz@users.noreply.github.com>
2020-03-07 10:21:02 +0100

    Update UserAccount.java

--- PayUp/src/default_package/UserAccount.java
+++ PayUp/src/default_package/UserAccount.java
@@ -14,20 +14,20 @@ public class UserAccount {
 	static Scanner userInput3 = new Scanner(System.in); // for double
 	int userChoice;
 	int loggedUserID;
-	
+
 	public UserAccount(String nameC, String passwordC, int uID ) {
 		userName = nameC;
 		password = passwordC;
 		userID = uID;
 	}
-	
+
 	public static UserAccount [] createAllUsers() {
 		int NumUser = 0;
 		String[] LocalUserName = new String[100];
 		String[] LocalPassword= new String[100];
 		String[] LocalTypeAccount = new String[100];
 		int[] LocalID = new int[100];
-		
+
 		UserAccount [] allUsers = new UserAccount [100];
 
 		try {
@@ -48,7 +48,7 @@ public class UserAccount {
 		}catch (IOException e) {
 			System.out.println("This file does not existlol5");
 		}
-		
+
 		String[] FinalLocalUserName = new String[NumUser];
 		System.arraycopy(LocalUserName, 0, FinalLocalUserName, 0, NumUser);
 		String[] FinalLocalPassword = new String[NumUser];
@@ -57,54 +57,82 @@ public class UserAccount {
 		System.arraycopy(LocalTypeAccount, 0, FinalLocalTypeAccount, 0, NumUser);
 		int[] FinalLocalID = new int[NumUser];
 		System.arraycopy(LocalID, 0, FinalLocalID, 0, NumUser);
-		
-		
+
+
 		for (int i = 0; i < NumUser; i++) {
 			allUsers [i]  = new UserAccount(FinalLocalUserName[i], FinalLocalPassword[i], FinalLocalID[i]);
 		}
-		
+
 		return allUsers;
 	}
-	
-	
+
+
 	public UserAccount(int userChoicec, int loggedIDc) {
-	userChoice = userChoicec;
-	loggedUserID = loggedIDc;
-	switch (userChoice){
-	case 3:
-		Exit();
-		break;
-	}
+		userChoice = userChoicec;
+		loggedUserID = loggedIDc;
+		switch (userChoice){
+		case 3:
+			Exit();
+			break;
+		}
 	}
 	public UserAccount() {
-		
+
 	}
 
-	protected static void Register() {
+	protected static void Register() { //This method does the registration for new users
 
 		noOfUsers = readfile1();
 		System.out.println("--------------------------------------------------------");
-		System.out.println("\t\t Please Register User");
+		System.out.println("\t Please Register User");
 		System.out.println("--------------------------------------------------------");
 		System.out.println("Are you registering as a (R) Regular or an (A) Administrator?");
-		String typeOfAccount = userInput2.nextLine();
-		System.out.println("Please choose a username:");
+		String typeOfAccount = userInput2.nextLine()
+		System.out.println("Please choose a username: ");
 		String Un = userInput2.nextLine();
-		System.out.println("Please choose a password:");
+		System.out.println("Please choose a password: ");
 		String Pw = userInput2.nextLine();
-		System.out.println("--------------------------------------------------------");
-		System.out.println("\t Thank you for registering to PayUp!");
-		System.out.println("--------------------------------------------------------");
 
-		try { //This is for Registration of the users
-			PrintWriter wr = new PrintWriter( new BufferedWriter(new FileWriter("User_database.txt",true)));
-			wr.println(Un + "\t" + Pw + "\t" + typeOfAccount + "\t" + noOfUsers);
-			wr.close();	
-		} catch (IOException e) {
-			System.out.println("I/O error when writing on file");
+		int length = Pw.length();
+		if (length < 4) {	//This disables new registrations with less than 4 characters
+			System.out.println("Your password must have 4 characters or more");
+			Register();	//User will be shown the register interface again for another attempt
 		}
-		new InterfaceClass();
+		else {	//When the user has entered a password that is more than 4 characters
+			try {	//This part tries to find whether the entered username already exists in the user database so duplicate names are avoided
+				int NumUser = 0;
+				String[] LocalUserName = new String[100];
+				String sCurrentLine;
+				String[] uCurrent = new String [4];
+				BufferedReader myFile = new BufferedReader (new FileReader("User_database.txt")); 
+				while ((sCurrentLine = myFile.readLine()) != null) {
+					uCurrent = sCurrentLine.split("\t");
+					LocalUserName[NumUser] = uCurrent[0];
+					NumUser++;
+					if(Un.equals(uCurrent[0])){
+						System.out.println("Username already exists, please try another one");
+					} else {	//If the entered username is unique a new row with user details will be written in the user database
+						try {
+							PrintWriter wr = new PrintWriter( new BufferedWriter(new FileWriter("User_database.txt",true)));
+							wr.println(Un + "\t" + Pw + "\t" + typeOfAccount + "\t" + noOfUsers);
+							wr.close();
+							System.out.println("--------------------------------------------------------");
+							System.out.println("\t Thank you for registering to PayUp!");
+							System.out.println("--------------------------------------------------------");
+							wr.close();
+
+						}catch (IOException e) {
+						}
+					}
+					new InterfaceClass();
+				}
+			}
+			catch (IOException e) {
+
+			}
+		}
 	}
+
 
 	protected static String[] Login() {
 		int NumUser = 0;
@@ -154,12 +182,12 @@ public class UserAccount {
 				System.out.println("--------------------------------------------------------");
 				System.out.println("Login successful, welcome " + Un +"!");
 			}
-			
+
 		}
 		String arrayofLoggedUser[] = new String[2];
 		arrayofLoggedUser[0] = Integer.toString(loggedID);
 		arrayofLoggedUser[1] = role;
-		
+
 		return arrayofLoggedUser;
 
 	}
