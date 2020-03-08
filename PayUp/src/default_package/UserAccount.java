23a02b2a56f4d5c01173bf4c07451e6756457baa
446901yz <61310280+446901yz@users.noreply.github.com>
2020-03-08 17:25:27 +0100

    Update UserAccount.java

--- PayUp/src/default_package/UserAccount.java
+++ PayUp/src/default_package/UserAccount.java
@@ -8,7 +8,6 @@ public class UserAccount {
 	String role;
 	static int noOfUsers;
 	int userID = 0;
-	private String sCurrentLine;
 	static Scanner userInput1 = new Scanner(System.in); //for int
 	static Scanner userInput2 = new Scanner(System.in); // for string
 	static Scanner userInput3 = new Scanner(System.in); // for double
@@ -22,7 +21,11 @@ public class UserAccount {
 		userID = uID;
 	}
 
-	public static UserAccount [] createAllUsers() {
+	public static UserAccount [] createAllUsers() {	
+		// This method reads the user database, creates an array of user objects containing user name, 
+		// password, type off account, and user ID
+		// The user account details are stored into separate arrays and then stored into 
+		// one all user array, which is what this method returns
 		int NumUser = 0;
 		String[] LocalUserName = new String[100];
 		String[] LocalPassword= new String[100];
@@ -81,12 +84,13 @@ public class UserAccount {
 
 	}
 
-	protected static void Register() {
+	protected static void Register() {	//This method gets the user name and password from the user for the registration of a new user
+
 
 		Boolean localBoolean = true;
 		noOfUsers = readfile1();
 		System.out.println("--------------------------------------------------------");
-		System.out.println("\t\t Please Register User");
+		System.out.println("\tPlease Register User\t");
 		System.out.println("--------------------------------------------------------");
 		System.out.println("Are you registering as a (R) Regular or an (A) Administrator?");
 		String typeOfAccount = userInput2.nextLine();
@@ -94,61 +98,60 @@ public class UserAccount {
 		String Un = userInput2.nextLine();
 		System.out.println("Please choose a password:");
 		String Pw = userInput2.nextLine();
-		
-		int pw = Pw.length();
-		
-		if (pw > 4) {
-		
-		UserAccount[] temp3 = UserAccount.createAllUsers();
 
-		for (int k = 0; k < noOfUsers; k++) {
-			if(Un.equals(temp3[k].userName)) {
-				localBoolean = false;
-			}else {
+		int pw = Pw.length();
+
+		if (pw > 4) { //This is to prevent passwords with less than 5 characters
+
+			UserAccount[] temp3 = UserAccount.createAllUsers();
+
+			for (int k = 0; k < noOfUsers; k++) {	//This is to check the uniqueness of entered user name within the user database
+				if(Un.equals(temp3[k].userName)) {
+					localBoolean = false;
+				}else {
+				}
 			}
-		}
-		System.out.println(localBoolean);
-		
-			if (localBoolean == true) {
+			System.out.println(localBoolean);
+
+			if (localBoolean == true) {	//If user name is unique a new line with the user's credentials will be written in the file
 				writeUser(Un, Pw, typeOfAccount);	
-			}else {
+			}else {	//The user will be redirected to the register interface due to a duplicate user name
 				System.out.println("--------------------------------------------------------");
 				System.out.println("This User Name is already taken, please Register again");
 				System.out.println("--------------------------------------------------------");
-				Register();
+				Register();	
 			}
-		}else {
+		}else {	//The user will be redirected to the register interface due to an invalid password
 			System.out.println("--------------------------------------------------------");
 			System.out.println("Please use a password of more than 4 characters");
 			System.out.println("--------------------------------------------------------");
-			Register();
+			Register();	
 		}
-		
-	}
-	
 
-	private static void writeUser(String Unc, String Pwc, String typeOfAccountc) {
+	}
+
+
+	private static void writeUser(String Unc, String Pwc, String typeOfAccountc) {	//This method writes a new line with the user's credentials in the user database txt file
 		String Un = Unc;
 		String Pw = Pwc;
 		String typeOfAccount = typeOfAccountc;
-		
-		try { //This is for Registration of the users
 
-				PrintWriter wr = new PrintWriter( new BufferedWriter(new FileWriter("User_database.txt",true)));
-				wr.println(Un + "\t" + Pw + "\t" + typeOfAccount + "\t" + noOfUsers);
-				wr.close();	
+		try {
+			PrintWriter wr = new PrintWriter( new BufferedWriter(new FileWriter("User_database.txt",true)));
+			wr.println(Un + "\t" + Pw + "\t" + typeOfAccount + "\t" + noOfUsers);
+			wr.close();	
 		}catch (IOException e) {
 			System.out.println("I/O error when writing on file");
 		}
-		
+		//The user will be redirected to the first menu after a successful registration
 		System.out.println("--------------------------------------------------------");
-		System.out.println("\t Thank you for registering to PayUp!");
+		System.out.println("\tThank you for registering to PayUp!\t");
 		System.out.println("--------------------------------------------------------");
 		new InterfaceClass();
 	}
-	
-	
-	protected static String[] Login() {
+
+
+	protected static String[] Login() {	//This method is for the login of users and returns the user's loggedID and role
 		int NumUser = 0;
 		String[] LocalUserName = new String[100];
 		String[] LocalPassword = new String[100];
@@ -158,16 +161,16 @@ public class UserAccount {
 		String Un;
 		String Pw;
 		String role = "bla";
-
+		//The login interface where the user enters his/her username and password
 		System.out.println("--------------------------------------------------------");
-		System.out.println("\t\t Please Login User");
+		System.out.println("\tPlease Login User\t");
 		System.out.println("--------------------------------------------------------");
 		System.out.println("Please input your username:");
 		Un = userInput2.nextLine();
 		System.out.println("Please input your password:");
 		Pw = userInput2.nextLine();
 
-		try {
+		try {	//This is for reading the user database file, sorting the data with user attributes, and storing the data into local arrays
 			String sCurrentLine;
 			String[] uCurrent = new String [4];
 			BufferedReader myFile = new BufferedReader (new FileReader("User_database.txt")); 
@@ -189,8 +192,8 @@ public class UserAccount {
 		System.arraycopy(LocalUserName, 0, FinalLocalUserName, 0, NumUser);
 		String[] FinalLocalPassword = new String[NumUser];
 		System.arraycopy(LocalPassword, 0, FinalLocalPassword, 0, NumUser);
-		for (int i = 0; i < NumUser; i++) {
-			if (Un.equals(FinalLocalUserName[i]) && Pw.equals(FinalLocalPassword[i])) {
+		for (int i = 0; i < NumUser; i++) {	//checking for every line in the user database if entered user name and password match with a row
+			if (Un.equals(FinalLocalUserName[i]) && Pw.equals(FinalLocalPassword[i])) {	
 				loggedID = LocalID[i];
 				role = LocalRole[i];
 				System.out.println("--------------------------------------------------------");
@@ -206,12 +209,12 @@ public class UserAccount {
 
 	}
 
-	protected static void Exit() {
-		System.out.println("--------------------------------------------------------");
-		System.out.println("\tYou are successfully logged out\t");
+	protected static void Exit() {	//This method will let the user exit PayUp
+		System.out.println("\n\tYou are successfully logged out\t");
+		System.out.println("\tThank you for visiting PayUp!\t");
 	}
 
-	public static int readfile1() {
+	public static int readfile1() {	// This method reads the user database file and returns all existing rows within the user database
 		int NumUser = 0;
 		try {
 			String sCurrentLine;
