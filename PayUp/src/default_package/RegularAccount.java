e0e44d82867fdd43611e8907db6e6da7a7672fe4
Sjoerd97 <61313847+Sjoerd97@users.noreply.github.com>
2020-03-09 11:59:57 +0100

    Update RegularAccount.java

--- PayUp/src/default_package/RegularAccount.java
+++ PayUp/src/default_package/RegularAccount.java
@@ -20,36 +20,6 @@ public class RegularAccount extends UserAccount{	//This is a subclass of the sup
 		role = roleC;
 
 	}
-	
-	public RegularAccount(int userChoicec, int loggedUserIDc) {	//This constructor contains a switch statement to 
-		//guide users to the right method after entering their preferred choice within the regularUserInterface method 
-	super();
-	userChoice = userChoicec;
-	loggedUserID = loggedUserIDc;
-	
-	switch (userChoice) { 
-	case 4:
-		viewIndividualBalance();
-		break;
-	case 5:
-		settleList();
-		break;
-	case 6:
-		addExpenseToList();
-		break;
-	case 9:
-		UserAccount.Exit();
-		break;
-		
-	default: 
-		System.out.println("--------------------------------------------------------");
-		System.out.println("------------------------CAUTION!------------------------");
-		System.out.println("          Please only enter a number between 0-8!        "); 
-		System.out.println("------------------------CAUTION!------------------------");
-		new InterfaceClass();
-
-	}
-	}
 
 	public static RegularAccount[] createRegulars() {	
 		// This method reads the user database and searches for regular users specifically
@@ -102,91 +72,5 @@ public class RegularAccount extends UserAccount{	//This is a subclass of the sup
 		}
 
 		return regulars;
-	}
-
-		
-
-	public void viewIndividualBalance() {	// This method prints out the user's individual balance 
-		System.out.println("Your invidivual balance is: " + Expense.splitExpense() + "€");
-	}
-
-
-	public static void settleList() {
-		settleList();
-	}
-
-	public void addExpenseToList() { 
-		//This method will write a new line onto the expense database every time users add an expense to their list
-		//This new line will contain the following user input: user's list ID, the name of the expense, 
-		//the expense amount, the expense date, and the user name of the person who paid for the expense. 
-		System.out.println("How many expenses do you want to add? ");
-		int counter = userInput1.nextInt();
-		for (int i = 0; i < counter; i++) {
-			System.out.println("Please choose the list ID of the list this expense belongs to: ");
-			List.listId = userInput1.nextInt();
-			System.out.println("Please choose an expense name: ");
-			Expense.expenseName = userInput2.nextLine();
-			System.out.println("Please choose the expense ammount: ");
-			Expense.amount = userInput3.nextDouble();
-			System.out.println("Please choose the expense date: ");
-			Expense.expenseDate = userInput2.nextLine();
-			System.out.println("Who paid for this expense? ");
-			UserAccount.userName = userInput2.nextLine();
-
-			try {
-				PrintWriter wr = new PrintWriter( new BufferedWriter(new FileWriter("Expense_database.txt", true)));
-				wr.println(List.listId + "\t" + Expense.expenseName + "\t" + Expense.amount + "\t" + Expense.expenseDate + "\t" + UserAccount.userName);
-				wr.close();	
-			} catch (IOException e) {
-				System.out.println("I/O error when writing on file");
-			}
-		}
-	}
-
-	public void viewExpenseHistory() { 
-		//This method prints out parts of the expense database that, together, represent the expense history of a user
-		int lineNumber = 0;
-		String[] c1 = new String[100];
-		String[] c2 = new String[100];
-		String[] c3 = new String[100];
-		String[] c4 = new String[100];
-		String[] c5 = new String[100];
-		System.out.println("List ID\tExpense Name\tAmount\tDate\tWho paid?");
-		try {
-			String sCurrentLine;
-			String[] uCurrent = new String [5];
-			BufferedReader myFile = new BufferedReader (new FileReader("Expense_database.txt")); 
-			while ((sCurrentLine = myFile.readLine()) != null) {
-				uCurrent = sCurrentLine.split("\t");
-
-				c1[lineNumber] = uCurrent[0];
-				c2[lineNumber] = uCurrent[1];
-				c3[lineNumber] = uCurrent[2];
-				c4[lineNumber] = uCurrent[3];
-				c5[lineNumber] = uCurrent[4];
-				lineNumber++;
-			}
-			myFile.close(); 
-		}catch (IOException e) {
-			System.out.println("This file does not existlol4");
-		}
-		String[] Finalc1 = new String[lineNumber];
-		System.arraycopy(c1, 0, Finalc1, 0, lineNumber);
-		String[] Finalc2 = new String[lineNumber];
-		System.arraycopy(c2, 0, Finalc2, 0, lineNumber);
-		String[] Finalc3 = new String[lineNumber];
-		System.arraycopy(c3, 0, Finalc3, 0, lineNumber);
-		String[] Finalc4 = new String[lineNumber];
-		System.arraycopy(c4, 0, Finalc4, 0, lineNumber);
-		String[] Finalc5 = new String[lineNumber];
-		System.arraycopy(c5, 0, Finalc5, 0, lineNumber);
-
-		for (int i = 0; i < Finalc1.length; i++) {
-			System.out.printf("%s\t%s\t%s\t%s\t%s\n", Finalc1[i], Finalc2[i], Finalc3[i], Finalc4[i], Finalc5[i]);
-		}
-	}
-
-	public void writeExpenseHistory() {
-		writeExpenseHistory();
 	}
 }
