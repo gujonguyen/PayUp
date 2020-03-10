e74cbd47d1727e15713c2d38d5433c80ae0063a8
TT3000 <61310353+TT3000@users.noreply.github.com>
2020-03-10 20:11:33 +0100

    Update AdministratorAccount.java

--- PayUp/src/default_package/AdministratorAccount.java
+++ PayUp/src/default_package/AdministratorAccount.java
@@ -9,15 +9,15 @@ public class AdministratorAccount extends UserAccount {
 	String typeOfAccount;
 	int userChoice;
 
-	public AdministratorAccount(String nameC, String passwordC, int uID, String roleC ) {
+	public AdministratorAccount(String nameC, String passwordC, int userID, String roleC ) {
 		/*
 		This is the main Constructor for the Administrator objects
 		 */
-		super(nameC, passwordC, uID);
+		super(nameC, passwordC, userID);
 		role = roleC;
 	}
 
-	public AdministratorAccount(int userChoicec, int loggedUserIDl) {
+	public AdministratorAccount(int userChoicec, int loggedUserIDLocal) {
 		/*
 		This Constructor directs the Admin User to the right method based on a passed-on Admin User Choice	
 		 */
@@ -41,46 +41,46 @@ public class AdministratorAccount extends UserAccount {
 		/*
 		This method creates Admin Objects
 		 */
-		int NumUser = 0;
-		String[] LocalUserName = new String[100];
-		String[] LocalPassword= new String[100];
-		String[] LocalTypeAccount = new String[100];
-		int[] LocalID = new int[100];
+		int numberOfUsers = 0;
+		String[] localUserName = new String[100];
+		String[] localPassword= new String[100];
+		String[] localTypeAccount = new String[100];
+		int[] localID = new int[100];
 
 		AdministratorAccount [] admins = new AdministratorAccount [100];
 
 		try {
-			String sCurrentLine;
-			String[] uCurrent = new String [4];
+			String currentLine;
+			String[] currentUser = new String [4];
 			BufferedReader myFile = new BufferedReader (new FileReader("User_database.txt")); 
-			while ((sCurrentLine = myFile.readLine()) != null) {
-				uCurrent = sCurrentLine.split("\t");
+			while ((currentLine = myFile.readLine()) != null) {
+				currentUser = currentLine.split("\t");
 
-				LocalUserName[NumUser] = uCurrent[0];
-				LocalPassword[NumUser] = uCurrent[1];
-				LocalTypeAccount[NumUser] = uCurrent[2];
-				LocalID[NumUser] = Integer.parseInt(uCurrent[3]);
+				localUserName[numberOfUsers] = currentUser[0];
+				localPassword[numberOfUsers] = currentUser[1];
+				localTypeAccount[numberOfUsers] = currentUser[2];
+				localID[numberOfUsers] = Integer.parseInt(currentUser[3]);
 
-				NumUser++;
+				numberOfUsers++;
 			}
 			myFile.close(); 
 		}catch (IOException e) {
 			System.out.println("This file does not existlol5");
 		}
 
-		String[] FinalLocalUserName = new String[NumUser];
-		System.arraycopy(LocalUserName, 0, FinalLocalUserName, 0, NumUser);
-		String[] FinalLocalPassword = new String[NumUser];
-		System.arraycopy(LocalPassword, 0, FinalLocalPassword, 0, NumUser);
-		String[] FinalLocalTypeAccount = new String[NumUser];
-		System.arraycopy(LocalTypeAccount, 0, FinalLocalTypeAccount, 0, NumUser);
-		int[] FinalLocalID = new int[NumUser];
-		System.arraycopy(LocalID, 0, FinalLocalID, 0, NumUser);
+		String[] finalLocalUserName = new String[numberOfUsers];
+		System.arraycopy(localUserName, 0, finalLocalUserName, 0, numberOfUsers);
+		String[] finalLocalPassword = new String[numberOfUsers];
+		System.arraycopy(localPassword, 0, finalLocalPassword, 0, numberOfUsers);
+		String[] finalLocalTypeAccount = new String[numberOfUsers];
+		System.arraycopy(localTypeAccount, 0, finalLocalTypeAccount, 0, numberOfUsers);
+		int[] finalLocalID = new int[numberOfUsers];
+		System.arraycopy(localID, 0, finalLocalID, 0, numberOfUsers);
 		int counterAdmins = 0;
 
-		for (int i = 0; i < NumUser; i++) {
-			if (FinalLocalTypeAccount[i].equals("R")) {
-				admins [counterAdmins]  = new AdministratorAccount(FinalLocalUserName[i], FinalLocalPassword[i],  FinalLocalID[i], FinalLocalTypeAccount[i]);
+		for (int i = 0; i < numberOfUsers; i++) {
+			if (finalLocalTypeAccount[i].equals("R")) {
+				admins [counterAdmins]  = new AdministratorAccount(finalLocalUserName[i], finalLocalPassword[i],  finalLocalID[i], finalLocalTypeAccount[i]);
 				counterAdmins ++;
 			}
 		}
@@ -90,17 +90,17 @@ public class AdministratorAccount extends UserAccount {
 
 	public void removeUser() {
 		//listing all the users of PayUp
-		int NumUser = 0;
+		int numberOfUsers = 0;
 		System.out.println("");
 		System.out.println("The list of users on PayUp");
 		System.out.println("----------------------------------------------------------------");
 
-		int numUser = UserAccount.readfile1();
+		int numberOfUsers1 = UserAccount.readfile1();
 		
 		UserAccount[] tempUserObjects = UserAccount.createAllUsers(); 
 
 		System.out.println("User ID \t\t User Name");
-		for(int k = 0; k < numUser; k++) {
+		for(int k = 0; k < numberOfUsers1; k++) {
 			System.out.println(tempUserObjects[k].userID + "\t\t\t" + tempUserObjects[k].userName);
 		}
 
@@ -114,7 +114,7 @@ public class AdministratorAccount extends UserAccount {
 		if(confirm.equals("Y")) {
 		try {
 			PrintWriter wr = new PrintWriter( new BufferedWriter(new FileWriter("User_database.txt",false)));
-			for (int i = 0; i < NumUser; i++) {
+			for (int i = 0; i < numberOfUsers; i++) {
 				if (i == localRemovedUser) {
 					wr.println("N/A" + "\t" + "N/A" + "\t" + "N/A" + "\t" + tempUserObjects[i].userID);
 					System.out.println("You have successfully deleted the User ID:" + localRemovedUser);
@@ -130,4 +130,3 @@ public class AdministratorAccount extends UserAccount {
 		}
 	}
 }
-
