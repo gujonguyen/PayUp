ccdadfe46607d8a3235525985ad60fad0169463c
Sjoerd97 <61313847+Sjoerd97@users.noreply.github.com>
2020-03-10 14:30:19 +0100

    Update AdministratorAccount.java

--- PayUp/src/default_package/AdministratorAccount.java
+++ PayUp/src/default_package/AdministratorAccount.java
@@ -89,67 +89,25 @@ public class AdministratorAccount extends UserAccount {
 	}
 
 	public void removeUser() {
-		/*
-		This Method enables the Admin to view a list of all the Regular users on PayUp.
-		From this list, the Admin can then choose which Regular user he wants to delete.	
-		 */
-		
-		// Declaring all necessary variables
-		boolean localBoolean = false;
+		//listing all the users of PayUp
 		int NumUser = 0;
-		String[] LocalUserName = new String[100];
-		String[] LocalPassword= new String[100];
-		String[] LocalTypeAccount = new String[100];
-		String [] LocalID = new String[100];
 		System.out.println("");
 		System.out.println("The list of users on PayUp");
 		System.out.println("----------------------------------------------------------------");
+
+		int numUser = UserAccount.readfile1();
 		
-		// Reading the User_database text file
-		try {
-			String sCurrentLine;
-			String[] uCurrentLine = new String[4];
-			BufferedReader br = new BufferedReader (new FileReader ("User_database.txt"));
+		UserAccount[] tempUserObjects = UserAccount.createAllUsers(); 
 
-			while ((sCurrentLine = br.readLine()) != null) {
-				uCurrentLine = sCurrentLine.split("\t");
-				LocalUserName[NumUser] = uCurrentLine[0];
-				LocalPassword[NumUser] = uCurrentLine[1];
-				LocalTypeAccount[NumUser] = uCurrentLine[2];
-				LocalID[NumUser] = uCurrentLine[3];
-				NumUser++;
-			}
-
-			br.close();
-		} catch (IOException e) {
-			System.out.println("The file does not existlol6");
-		}
-		
-		//This creates an array of all registered Regular users
-		String[] FinalLocalUserName = new String[NumUser];
-		System.arraycopy(LocalUserName, 0, FinalLocalUserName, 0, NumUser);
-		String[] FinalLocalPassword = new String[NumUser];
-		System.arraycopy(LocalPassword, 0, FinalLocalPassword, 0, NumUser);
-		String[] FinalLocalTypeAccount = new String[NumUser];
-		System.arraycopy(LocalTypeAccount, 0, FinalLocalTypeAccount, 0, NumUser);
-		String[] FinalLocalID = new String[NumUser];
-		System.arraycopy(LocalID, 0, FinalLocalID, 0, NumUser);
-
-		// Printing out the arrays that were created of all user ID's and their corresponding user names
-		System.out.println("UserName" + "\t\t UserList");
-		for(int k = 0; k < NumUser; k++) {
-			System.out.println(LocalID[k] + "\t" + "\t" + LocalUserName[k]);
+		System.out.println("User ID \t\t User Name");
+		for(int k = 0; k < numUser; k++) {
+			System.out.println(tempUserObjects[k].userID + "\t\t" + tempUserObjects[k].userName);
 		}
 
-		// Asking the Admin which user he wants to remove
+		//removing the user from PayUp
 		System.out.println("");
 		System.out.println("Enter the User ID you wish to remove admin: ");
 		int localRemovedUser = userInput1.nextInt() ;
-		
-		/*
-		Asking the Admin to confirm to delete the user and printing Not Available (N/A) 
-		in User_database text file for that particular user
-		 */
 		System.out.println("Are you sure? Deleting users is permanent and the user will not be able to login anymore (Y/N)");
 		String confirm = userInput2.nextLine();
 
@@ -158,10 +116,10 @@ public class AdministratorAccount extends UserAccount {
 			PrintWriter wr = new PrintWriter( new BufferedWriter(new FileWriter("User_database.txt",false)));
 			for (int i = 0; i < NumUser; i++) {
 				if (i == localRemovedUser) {
-					wr.println("N/A" + "\t" + "N/A" + "\t" + "N/A" + "\t" + FinalLocalID[i]);
+					wr.println("N/A" + "\t" + "N/A" + "\t" + "N/A" + "\t" + tempUserObjects[i].userID);
 					System.out.println("You have successfully deleted the User ID:" + localRemovedUser);
 				} else {
-					wr.println(FinalLocalUserName[i] + "\t" + FinalLocalPassword[i] + "\t" + FinalLocalTypeAccount[i] + "\t" + FinalLocalID[i]);	
+					wr.println(tempUserObjects[i].userName + "\t" + tempUserObjects[i].password + "\t" + tempUserObjects[i].role + "\t" + tempUserObjects[i].userID);	
 				}
 			}
 			wr.close();
